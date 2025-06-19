0e4b8b27779c4a321feb7f1ad8b96192a8d80b4b
Seungjin Bae <eeodqql09@gmail.com>
2025-06-19 01:57:47 -0400

    usb: host: xhci-plat: fix incorrect type for of_match variable in xhci_plat_probe()
    [ Upstream commit d9e496a9fb4021a9e6b11e7ba221a41a2597ac27 ]
    
    The variable `of_match` was incorrectly declared as a `bool`.
    It is assigned the return value of of_match_device(), which is a pointer of
    type `const struct of_device_id *`.
    
    Fixes: 16b7e0cccb243 ("USB: xhci-plat: fix legacy PHY double init")
    Signed-off-by: Seungjin Bae <eeodqql09@gmail.com>
    Link: https://lore.kernel.org/r/20250619055746.176112-2-eeodqql09@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 9b49f02bfc67c18d9afdf9b6d7d098f5dcd84848)

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -220,6 +220,7 @@ static int xhci_plat_probe(struct platform_device *pdev)
 	int			irq;
 	struct xhci_plat_priv	*priv = NULL;
 
+	const struct of_device_id *of_match;
 
 	if (usb_disabled())
 		return -ENODEV;
